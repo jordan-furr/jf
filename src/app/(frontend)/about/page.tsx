7ea0a38b1967b608c400c3257a4399f030dd6238
jordan-furr <jordanfurr98@gmail.com>
2025-10-01 22:59:24 -0400

    eslint fix

--- src/app/(frontend)/about/page.tsx
+++ src/app/(frontend)/about/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
           className="mb3"
           priority
         />
-        <p className="mb3">Jordan Ellis Furr is an xxxtra multi & passionate creator! Based in Brooklyn, he creates web art, websites, essays, textiles, videos, and more. He would love to hear from you to discuss, become penpals, collaborate, dream up a website, or something else. Please don't hesitate to send him an email.</p>
+        <p className="mb3">Jordan Ellis Furr is an xxxtra multi & passionate creator! Based in Brooklyn, he creates web art, websites, essays, textiles, videos, and more. He would love to hear from you to discuss, become penpals, collaborate, dream up a website, or something else. Please don&apos;t hesitate to send him an email.</p>
         <p>jordan@jordanfurr.com</p>
       </div>
     </div>
